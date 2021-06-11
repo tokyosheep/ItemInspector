6d41b9cb808237028c9f8a1fdda86ffb222607fb
Shuji <tokyosheep@hotmail.com>
2021-06-11 19:02:03 +0900

    modified

--- jsx/singleProcess/getAllplaces.jsx
+++ jsx/singleProcess/getAllplaces.jsx
@@ -2,7 +2,7 @@
     var places = app.activeDocument.placedItems;
     var files = [];
     for(var i=0;i<places.length;i++){
-        files[i] = places[i].file.toString();
+        files[i] = decodeURI(places[i].file.toString());
     }
     return JSON.stringify({files:files,doc:app.activeDocument.fullName.toString()});
 })();
