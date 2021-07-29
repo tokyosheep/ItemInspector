19c64fb7df1282c61926e9ceab928d85131f4a47
Shuji <tokyosheep@hotmail.com>
2021-07-29 22:32:48 +0900

    update readme

--- test/readEps.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-
-(async()=>{
-    const filePath = process.argv[2];
-    console.log(filePath);
-    const content = await fs.promises.readFile(filePath);
-    console.log(content);
-})();
